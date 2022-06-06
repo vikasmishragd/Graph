55c85b36066ed674c4504fe77fee98d3a998f838
vikasmishragd <65272228+vikasmishragd@users.noreply.github.com>
2022-06-06 20:32:57 +0200

    Add project files.

---
+++ Graph.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph11
+{
+    public class Graph
+    {
+
+        Dictionary<string, List<string>> adjecencyList;
+        public Graph()
+        {
+            adjecencyList = new Dictionary<string, List<string>>();
+        }
+
+        public void CreateConnection(string source, string destination)
+        {   List<string> values;
+            if(adjecencyList.ContainsKey(source))
+            {
+                adjecencyList.TryGetValue(source, out values);
+                if (!values.Contains(destination))
+                    values.Add(destination);
+                adjecencyList.Remove(source);
+                adjecencyList.Add(source, values);
+            }
+            else
+                adjecencyList.Add(source, new List<string> { destination});
+        }
+        public void CreateConnectionUndirected(string source, string destination)
+        {
+            List<string> values;
+            if (adjecencyList.ContainsKey(source))
+            {
+                adjecencyList.TryGetValue(source, out values);
+                if (!values.Contains(destination))
+                    values.Add(destination);
+                adjecencyList.Remove(source);
+                adjecencyList.Add(source, values);
+            }
+            else
+            {
+                if (source != null && !adjecencyList.ContainsKey(source))
+                    adjecencyList.Add(source, new List<string> { destination });
+            }
+            if (destination != null && adjecencyList.ContainsKey(destination))
+            {
+                adjecencyList.TryGetValue(destination, out values);
+                if (!values.Contains(source))
+                    values.Add(source);
+                adjecencyList.Remove(destination);
+                adjecencyList.Add(destination, values);
+            }
+            else
+            {
+                if(destination != null && !adjecencyList.ContainsKey(destination) )
+                adjecencyList.Add(destination, new List<string> { source });
+
+                //adjecencyList.TryGetValue(destination, out values);
+                //if (!values.Contains(source))
+                //    values.Add(source);
+                //adjecencyList.Remove(destination);
+                //adjecencyList.Add(destination, values);
+            }
+        }
+
+        public void DFS(string startNode)
+        {
+            var stack = new Stack<string>();
+            stack.Push(startNode);
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+
+                Console.WriteLine(current);
+                adjecencyList.TryGetValue(current, out List<string> neighoburs);
+                foreach (var nodes in neighoburs)
+                {
+                    if (nodes != null)
+                        stack.Push(nodes);
+                }
+
+            }
+        }
+
+        public void BFS(string startNode)
+        {
+            var queue = new Queue<string>();
+            queue.Enqueue(startNode);
+            while(queue.Count >0)
+            {
+                var current = queue.Dequeue();
+                Console.WriteLine(current);
+                adjecencyList.TryGetValue(current, out List<string> neighoburs);
+                foreach (var nodes in neighoburs)
+                {
+                    if (nodes != null)
+                        queue.Enqueue(nodes);
+                }
+
+            }
+        }
+
+        public void DFSRecursion(string startNode)
+        {
+            Console.WriteLine(startNode);
+            adjecencyList.TryGetValue(startNode, out List<string> neighoburs);
+            foreach (var item in neighoburs)
+            {
+                if (item != null)
+                    DFSRecursion(item);
+            }
+        }
+
+        public bool PathExistsBetweenVertexs(string source, string destination)
+        {
+            var queue = new Queue<string>();
+            queue.Enqueue(source);
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                Console.WriteLine(current);
+                adjecencyList.TryGetValue(current, out List<string> neighoburs);
+                foreach (var nodes in neighoburs)
+                {
+                    if (nodes != null)
+                        queue.Enqueue(nodes);
+
+                    if(queue.Contains(destination))
+                    {
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+
+        public bool PathExistsRecursion(string source, string detination)
+        {
+            if (source == detination)
+                return true;
+            adjecencyList.TryGetValue(source, out List<string> neighoburs);
+
+            foreach(string node in neighoburs)
+            {
+                if(node != null && PathExistsRecursion(node,detination))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Graph11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph();
+
+
+            //graph.CreateConnection("A", "C");
+            //graph.CreateConnection("A", "B");
+            //graph.CreateConnection("B", "D");
+            //graph.CreateConnection("C", "E");
+            //graph.CreateConnection("D", "F");
+            //graph.CreateConnection("E", null);
+            //graph.CreateConnection("F", null);
+            //           A
+            //          / \
+            //         B   C
+            //        /     \
+            //       D      E
+            //      /
+            //     F
+            //graph.CreateConnection("F", "G");
+            //graph.CreateConnection("F", "I");
+            //graph.CreateConnection("G", "H");
+            //graph.CreateConnection("H", null);
+            //graph.CreateConnection("I", "G");
+            //graph.CreateConnection("I", "K");
+            //graph.CreateConnection("J", "I");
+            //graph.CreateConnection("K", null);
+
+
+            graph.CreateConnectionUndirected("F", "G");
+            graph.CreateConnectionUndirected("F", "I");
+            graph.CreateConnectionUndirected("G", "H");
+            graph.CreateConnectionUndirected("H", null);
+            graph.CreateConnectionUndirected("I", "G");
+            graph.CreateConnectionUndirected("I", "K");
+            graph.CreateConnectionUndirected("J", "I");
+            graph.CreateConnectionUndirected("K", null);
+
+
+            // graph.BFS("A");
+            // Console.WriteLine(graph.PathExistsBetweenVertexs("J", "F"));
+           // Console.WriteLine(graph.PathExistsRecursion("J","H"));
+        }
+    }
+}
